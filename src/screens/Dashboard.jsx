c6f2f70945c60e2a1de142b4ba2866f829d97375
omeryasind7 <omeryasind7@gmail.com>
2025-01-26 17:55:35 +0300

    logout

--- src/screens/Dashboard.jsx
+++ src/screens/Dashboard.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import Siderbar from '../components/Siderbar'
+import { lsToken } from '../data/lsToken'
+import { useNavigate } from 'react-router-dom';
 const Dashboard = ({isAdmin}) => {
+  
+  const navigate = useNavigate();
+  
   return (
     <div style={{display: "flex"}}>
       <Siderbar isAdmin={isAdmin} selectedItem="dashboard" />
-      <div>Dashboard</div>
+      <div style={{padding: "16px"}}>
+        <button onClick={() => {
+          localStorage.removeItem(lsToken);
+          navigate("/login");
+          window.location.reload();
+        }} style={{minWidth: "100px"}} className='tButton'>Çıkış Yap</button>
+      </div>
     </div>
   )
 }
