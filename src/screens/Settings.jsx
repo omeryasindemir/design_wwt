e73a71ea74e024fad247c7a9c004199df634e01c
omeryasind7 <omeryasind7@gmail.com>
2025-02-20 13:57:43 +0300

    settings

--- src/screens/Settings.jsx
+++ src/screens/Settings.jsx
@@ -2,10 +2,12 @@ import React, { useState } from 'react'
 import Siderbar from '../components/Siderbar'
 import { lsToken } from '../data/lsToken'
 import { userCredSave } from '../server/request'
-const Settings = ({userData, isAdmin}) => {
+const Settings = ({ userData, isAdmin }) => {
 
   const [tckn, settckn] = useState(userData?.tckn)
   const [password, setpassword] = useState(userData?.password)
+  const [cookie, setcookie] = useState(userData?.cookie)
+  const [copyText, setCopyText] = useState('Kopyala')
 
 
   const handleSave = async () => {
@@ -19,6 +21,19 @@ const Settings = ({userData, isAdmin}) => {
     } catch (error) {
       console.log(error)
     }
+  }
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(cookie)
+      .then(() => {
+        setCopyText('Kopyalandı!')
+        setTimeout(() => {
+          setCopyText('Kopyala')
+        }, 2000)
+      })
+      .catch(err => {
+        console.error('Kopyalama işlemi başarısız:', err)
+      })
   }
 
   return (
@@ -41,16 +56,35 @@ const Settings = ({userData, isAdmin}) => {
             borderTopRightRadius: "12px",
             fontWeight: "400"
           }}>İhale Sistemi Ayarları</div>
-          <div style={{padding: "16px"}}>
+          <div style={{ padding: "16px" }}>
             <div>
               <input onChange={(e) => settckn(e.target.value)} value={tckn} className='tInput' type="number" name="tckn" id="tckn" placeholder="T.C. Kimlik No" />
             </div>
-            <div style={{marginTop: "16px"}}>
+            <div style={{ marginTop: "16px" }}>
               <input onChange={(e) => setpassword(e.target.value)} value={password} className='tInput' type="text" name="password" id="password" placeholder="Parola" />
             </div>
           </div>
-          <div style={{padding: "16px"}}>
+          <div style={{ padding: "16px" }}>
             <button onClick={handleSave} className='tButton'>Kaydet</button>
+          </div>
+        </div>
+        <div style={{ marginTop: "24px", border: "1px solid var(--border-color)", borderRadius: "12px", backgroundColor: "var(--white-color)", width: "100%", maxWidth: "480px" }}>
+          <div style={{
+            height: "48px",
+            padding: "16px",
+            borderBottom: "1px solid var(--border-color)",
+            backgroundColor: "var(--bg-color-light)",
+            borderTopLeftRadius: "12px",
+            borderTopRightRadius: "12px",
+            fontWeight: "400"
+          }}>Cookie</div>
+          <div style={{ padding: "16px" }}>
+            <div>
+              <input onChange={(e) => setcookie(e.target.value)} disabled={true} value={cookie} className='tInput' type="text" name="cookie" id="cookie" placeholder="Cookie" />
+            </div>
+          </div>
+          <div style={{ padding: "16px" }}>
+            <button onClick={handleCopy} className='tButton'>{copyText}</button>
           </div>
         </div>
       </div>
