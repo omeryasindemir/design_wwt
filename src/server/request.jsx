fc1fa70951e54c3f8291a2dd5057dbe16f2325e8
omeryasindemir <omeryasindemir66@gmail.com>
2025-06-20 10:10:26 +0300

    deployyy

--- src/server/request.jsx
+++ src/server/request.jsx
@@ -1,5 +1,5 @@
 // requests.jsx
-const API_BASE_URL = "https://19eb-46-1-105-166.ngrok-free.app";
+const API_BASE_URL = "https://d247-46-1-27-7.ngrok-free.app";
 
 export const getRequest = async (endpoint) => {
     try {
